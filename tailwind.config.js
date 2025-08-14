56847187d053d7c5a7e2a6af155b11282b206c30
M.Robi <mrobichaniago@gmail.com>
2025-08-14 21:01:35 +0700

    project configure and tailwind with live update view

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts}"],
+  theme: {
+    extend: {
+      // optional: custom screens if you want a named 300px breakpoint
+      screens: { xxs: "300px" },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
